9de17f126f5db1b4e8ee29b48915cfcbde75a045
wll <iwanglulu@gmail.com>
2016-05-12 01:40:58 +0800

    update data_abs/basic.scm

--- scheme/data_abstract/basic.scm
+++ scheme/data_abstract/basic.scm
@@ -1,4 +1,5 @@
 
+
 ;仅用过程实现cons,car,cdr
 
 (define (mycons x y)
@@ -24,24 +25,93 @@
 	 	(func 1 (cons x li))))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;sequence operaton!
+
+
+
 ;programming in sequence transforming
+;----------------------------------------------------------
 
-;generate a sequence
+; 1.generate a sequence
+;---pass
 
-;使用map
+; 2(a).使用map进行转换
 (define (squire_list li)
 	(map (lambda (x) (* x x)) li))
 
 
-;使用filter
+; 2(b).使用filter进行过滤
 (filter odd? (list 1 2 3 4 5 6))
 
 
-;accumulate
+; 3.accumulate
 (fold + 0 (list 1 2 3 4))
 
 
+
+;例子:求二叉树的值为奇数的叶子结点的平方和
+;1.generate a seq: transform a tree to a seq
+
+(define tr (list 1 (list 2 3) (list 4 5)))
+
+(define (eunm_tree tree)
+	(cond 	((null? tree) ())
+	      	((not (pair? tree)) (cons tree ()))
+	      	(else (append 
+	      				(eunm_tree (car tree)) 
+	      				(eunm_tree (cdr tree))))))
+
+
+;2.使用filter过滤留下odd数
+
+(define ftr (filter odd? (eunm_tree tr)))
+
+;3.使用map进行square
+
+(define mftr (map square ftr))
+
+;4.accumulate
+
+(define ans (fold + 0 mftr))
+
+
+;将2.3.4合在一起
+(define ans2 (fold 	+ 
+					0 
+					(map square
+						 (filter odd?
+						 		 (eunm_tree tr)))))
+
+
+
+;;;exercise2.33
+(define (myfold f e seq)
+	(cond ((null? seq) e)
+	      (else (f  (car seq)
+	      			(myfold f e (cdr seq))))))
+
+(define (mymap f seq)
+	(myfold (lambda (x y) 
+				(cons (f x) y)) 
+		  () 
+		  seq))
+
+(define (myappend ls1 ls2)
+	(myfold cons 
+		  ls2
+		  ls1))
+
+
+(define (mylength seq)
+	(myfold (lambda (x y)
+					(+ 1 y))
+			0
+			seq))
+
+;;;exercise2.37
+
+;;;exercise2.38
+
+;;;exercise2.39
 
 
 
