cb510d56e4cade91f24eff8f78701b02410c6d87
wll <iwanglulu@gmail.com>
2016-06-01 23:25:05 +0800

    Update evaluator.rkt

--- scheme/4.metalinhuistic-abstraction/evaluator.rkt
+++ scheme/4.metalinhuistic-abstraction/evaluator.rkt
@@ -3,10 +3,26 @@
 ;eval apply loop
 (define (eval exp env)
   ())
+
   
 (define (apply proc args)
   ())
 
+
+(define (list-of-values exps env)
+  ())
+
+(define (eval-if exp env)
+  ())
+
+(define (eval-sequence exps env)
+  ())
+
+(define (eval-assignment exp evn)
+  ())
+
+(define (eval-definition exp env)
+  ())
 
 
 
