6b4158eef9c89a7cafc246f736a50660897a9cd3
wll <iwanglulu@gmail.com>
2016-06-03 00:18:24 +0800

    update

--- scheme/4.metalinhuistic-abstraction/evaluator.rkt
+++ scheme/4.metalinhuistic-abstraction/evaluator.rkt
@@ -14,12 +14,22 @@
   					   env))
   		((begin? exp) 
   			(eval-sequence (begin-actions exp) env))
-  		()
-  		()))
+  		((cond? exp) (eval (cond->if exp) env))
+  		((application? exp)
+  			(apply (eval (operator exp) env)
+  				   (list-of-values (operands exp) env)))
+  		(else (error "Unknow expression type -- EVAL" exp))))
 
   
 (define (apply proc args)
-  ())
+  (cond ((primitive-proc? proc)
+  			(apply-primitive-proc proc args))
+  		((compound-proc? proc)
+  			(eval-sequence (proc-body proc)
+  						   (extend-env  (proc-params proc)
+  						   				args
+  						   				(proc-env proc))))
+  		(else (error "Unknow procedual type -- APPLY" proc))))
 
 
 (define (list-of-values exps env)
