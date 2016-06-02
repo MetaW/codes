fbbae6e7619768b3944a01eda26ef32619d7832f
wll <iwanglulu@gmail.com>
2016-06-02 23:57:33 +0800

    update

--- scheme/4.metalinhuistic-abstraction/evaluator.rkt
+++ scheme/4.metalinhuistic-abstraction/evaluator.rkt
@@ -2,7 +2,20 @@
 
 ;eval apply loop
 (define (eval exp env)
-  ())
+  (cond ((self-evaluating? exp) exp)
+  		((variable? exp) (lookup-var-value exp evn))
+  		((quoted? exp) (text-of-quotation exp))
+  		((assignment? exp) (eval-assignment exp env))
+  		((definition? exp) (eval-definition exp env))
+  		((if? exp) (eval-if exp evn)) 
+  		((lambda? exp)
+  			(make-proc (lambda-params exp)
+  					   (lambda-body exp)
+  					   env))
+  		((begin? exp) 
+  			(eval-sequence (begin-actions exp) env))
+  		()
+  		()))
 
   
 (define (apply proc args)
